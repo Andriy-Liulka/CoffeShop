eef39db656d263ac82fddfd7ce58404090c98197
Andriy <andriy0liulka@gmail.com>
2022-07-28 16:56:52 +0300

    Added configuration.

--- CoffeShop/CoffeeShop.Api/Startup.cs
+++ CoffeShop/CoffeeShop.Api/Startup.cs
@@ -4,6 +4,12 @@ namespace CoffeShop.Api;
 
 public class Startup
 {
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    public IConfiguration Configuration { get; }
+    
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddLogging();
