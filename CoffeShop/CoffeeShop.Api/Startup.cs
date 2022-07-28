b50d9c22dea41acb637ddc17e1be5b390413a43b
Andriy <andriy0liulka@gmail.com>
2022-07-28 16:30:54 +0300

    Deleted useless pieces of code.

--- CoffeShop/CoffeeShop.Api/Startup.cs
+++ CoffeShop/CoffeeShop.Api/Startup.cs
@@ -4,13 +4,9 @@ namespace CoffeShop.Api;
 
 public class Startup
 {
-    public Startup(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
-    public IConfiguration Configuration { get; }
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddLogging();
         services.AddControllers();
         services.AddSwaggerGen(c =>
         {
