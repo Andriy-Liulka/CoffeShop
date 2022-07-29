e64f98963ce5d3da9ad1d43e8c380c1a1e5b3b55
Andriy <andriy0liulka@gmail.com>
2022-07-29 17:55:51 +0300

    Added many to many relationships.

---
+++ CoffeShop/CoffeeShop.Domain/Entities/MtM_IntermediateEntities/Discount_Coffee.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeShop.Domain.Entities.MtM_IntermediateEntities;
+
+public class Discount_Coffee
+{
+    public long DiscountId { get; set; }
+    public virtual Discount Discount { get; set; }
+    
+    public long CoffeetId { get; set; }
+    public virtual Coffee Coffee { get; set; }
+}
---
+++ CoffeShop/CoffeeShop.Domain/Entities/MtM_IntermediateEntities/Order_Coffee.cs
@@ -0,0 +1,13 @@
+﻿namespace CoffeeShop.Domain.Entities.MtM_IntermediateEntities;
+
+public class Order_Coffee
+{
+    public long OrderId { get; set; }
+    public virtual Order Order { get; set; }
+    
+    public long VolumeId { get; set; }
+    public virtual Volume Volume { get; set; }
+    
+    public long CoffeetId { get; set; }
+    public virtual Coffee Coffee { get; set; }
+}
