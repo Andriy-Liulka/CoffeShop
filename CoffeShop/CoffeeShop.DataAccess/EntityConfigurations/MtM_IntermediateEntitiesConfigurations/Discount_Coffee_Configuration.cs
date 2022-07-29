c04f17d7c732aeb2d423b2914684ddde7f8b16d3
Andriy <andriy0liulka@gmail.com>
2022-07-29 17:59:06 +0300

    added mtm file configuration files.

---
+++ CoffeShop/CoffeeShop.DataAccess/EntityConfigurations/MtM_IntermediateEntitiesConfigurations/Discount_Coffee_Configuration.cs
@@ -0,0 +1,17 @@
+﻿using CoffeeShop.DataAccess.Common;
+using CoffeeShop.Domain.Entities;
+using CoffeeShop.Domain.Entities.MtM_IntermediateEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoffeeShop.DataAccess.EntityConfigurations.MtM_IntermediateEntitiesConfigurations;
+
+public class Discount_Coffee_Configuration : IEntityConfiguration<Discount_Coffee>
+{
+    public EntityTypeBuilder<Discount_Coffee> Configure(EntityTypeBuilder<Discount_Coffee> builder)
+    {
+        builder.ToTable(TableNameCreator.CreateDefaultTableName(()=>nameof(Discount_Coffee)));
+
+        return builder;
+    }
+}
---
+++ CoffeShop/CoffeeShop.DataAccess/EntityConfigurations/MtM_IntermediateEntitiesConfigurations/Order_Coffee_Configuration.cs
@@ -0,0 +1,16 @@
+﻿using CoffeeShop.DataAccess.Common;
+using CoffeeShop.Domain.Entities.MtM_IntermediateEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoffeeShop.DataAccess.EntityConfigurations.MtM_IntermediateEntitiesConfigurations;
+
+public class Order_Coffee_Configuration : IEntityConfiguration<Order_Coffee>
+{
+    public EntityTypeBuilder<Order_Coffee> Configure(EntityTypeBuilder<Order_Coffee> builder)
+    {
+        builder.ToTable(TableNameCreator.CreateDefaultTableName(()=>nameof(Order_Coffee)));
+
+        return builder;
+    }
+}
