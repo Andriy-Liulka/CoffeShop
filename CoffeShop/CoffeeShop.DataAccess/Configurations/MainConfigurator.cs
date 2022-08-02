72cc5761c55b34b84cd1e7c9fea2507b33975b90
Andriy <andriy0liulka@gmail.com>
2022-08-03 00:29:38 +0300

    Added SetDafaultData method to MainConfigurator.

--- CoffeShop/CoffeeShop.DataAccess/Configurations/MainConfigurator.cs
+++ CoffeShop/CoffeeShop.DataAccess/Configurations/MainConfigurator.cs
@@ -5,8 +5,13 @@ namespace CoffeeShop.DataAccess.Configurations;
 
 public static class Configurator
 {
-    public static void Configure<TEntity,TEntityType>(this EntityTypeBuilder<TEntityType> entity) 
+    public static EntityTypeBuilder<TEntityType> Configure<TEntity,TEntityType>(this EntityTypeBuilder<TEntityType> entity) 
         where TEntity : IEntityConfiguration<TEntityType>,new()
         where TEntityType : class
     =>new TEntity().Configure(entity);
+    
+    public static EntityTypeBuilder<TEntityType> SetDefaultData<TEntity,TEntityType>(this EntityTypeBuilder<TEntityType> entity) 
+        where TEntity : IDefaultDataSetter<TEntityType>,new()
+        where TEntityType : class
+        =>new TEntity().SetDefaultData(entity);
 }
