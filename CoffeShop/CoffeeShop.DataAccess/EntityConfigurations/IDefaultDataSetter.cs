f92bf7999a233fca658fe21416e91d302c44a3b6
Andriy <andriy0liulka@gmail.com>
2022-08-03 00:30:33 +0300

    Added IDefaultDataSetter + SetDefaultData.

---
+++ CoffeShop/CoffeeShop.DataAccess/EntityConfigurations/IDefaultDataSetter.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoffeeShop.DataAccess.EntityConfigurations;
+
+public interface IDefaultDataSetter<TEntityType> where TEntityType:class
+{
+    public EntityTypeBuilder<TEntityType> SetDefaultData(EntityTypeBuilder<TEntityType> builder);
+}
