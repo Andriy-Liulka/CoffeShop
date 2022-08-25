e46a3496b4e02c3c25552006e1e4ef72423c4b2b
Andrii <nagibator7337@gmail.com>
2022-08-26 00:09:58 +0300

    Implemented BonusCoffeeService and DiscountService.

--- CoffeShop/CoffeeShop.BusinessLogic/MainBusinessLogic/Services/BonusCoffeeService.cs
+++ CoffeShop/CoffeeShop.BusinessLogic/MainBusinessLogic/Services/BonusCoffeeService.cs
@@ -1,4 +1,6 @@
 ﻿using CoffeeShop.DataAccess;
+using CoffeeShop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoffeeShop.BusinessLogic.MainBusinessLogic;
 
@@ -9,5 +11,51 @@ public class BonusCoffeeService
     public BonusCoffeeService(CoffeeShopContext context)
     {   
         _context = context;
+    }
+    
+    public async Task<List<BonusCoffee>> GetAllAsync() => await _context.BonusCoffees
+        .Include(x=>x.Coffee)
+        .ThenInclude(x=>x.BonusCoffees)
+        
+        .Include(x=>x.Coffee)
+        .ThenInclude(x=>x.DiscountCoffees)
+        
+        .Include(x=>x.Coffee)
+        .ThenInclude(x=>x.OrderVolumeCoffees)
+        
+        .Include(x=>x.Volume)
+
+        .ToListAsync();
+
+    public async Task<BonusCoffee?> GetAsync(int id) => await _context.BonusCoffees
+        .Include(x=>x.Coffee)
+        .ThenInclude(x=>x.BonusCoffees)
+        
+        .Include(x=>x.Coffee)
+        .ThenInclude(x=>x.DiscountCoffees)
+        
+        .Include(x=>x.Coffee)
+        .ThenInclude(x=>x.OrderVolumeCoffees)
+        
+        .Include(x=>x.Volume)
+
+        .FirstOrDefaultAsync(x => x.Id.Equals(id));
+
+    public async Task CreateAsync(BonusCoffee bonusCoffee)
+    {
+        await _context.BonusCoffees.AddAsync(bonusCoffee);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(BonusCoffee bonusCoffee)
+    {
+        _context.BonusCoffees.Update(bonusCoffee);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(BonusCoffee bonusCoffee)
+    {
+        _context.BonusCoffees.Remove(bonusCoffee);
+        await _context.SaveChangesAsync();
     }
 }
--- CoffeShop/CoffeeShop.BusinessLogic/MainBusinessLogic/Services/DiscountService.cs
+++ CoffeShop/CoffeeShop.BusinessLogic/MainBusinessLogic/Services/DiscountService.cs
@@ -1,6 +1,51 @@
-﻿namespace CoffeeShop.BusinessLogic.MainBusinessLogic;
+﻿using CoffeeShop.DataAccess;
+using CoffeeShop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.BusinessLogic.MainBusinessLogic;
 
 public class DiscountService
 {
+    private readonly CoffeeShopContext _context;
+
+    public DiscountService(CoffeeShopContext context)
+    {   
+        _context = context;
+    }
     
+    public async Task<List<Discount>> GetAllAsync() => await _context.Discounts
+        .Include(x=>x.DiscountCoffees)
+        .ThenInclude(x=>x.Coffee)
+
+        .Include(x=>x.DiscountCoffees)
+        .ThenInclude(x=>x.Discount)
+        
+        .ToListAsync();
+
+    public async Task<Discount?> GetAsync(int id) => await _context.Discounts
+        .Include(x=>x.DiscountCoffees)
+        .ThenInclude(x=>x.Coffee)
+
+        .Include(x=>x.DiscountCoffees)
+        .ThenInclude(x=>x.Discount)
+
+        .FirstOrDefaultAsync(x => x.Id.Equals(id));
+
+    public async Task CreateAsync(Discount discount)
+    {
+        await _context.Discounts.AddAsync(discount);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Discount discount)
+    {
+        _context.Discounts.Update(discount);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Discount discount)
+    {
+        _context.Discounts.Remove(discount);
+        await _context.SaveChangesAsync();
+    }
 }
