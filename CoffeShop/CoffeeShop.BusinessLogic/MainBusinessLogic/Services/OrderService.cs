2ccf06428d995ef5bffdadbef52b4d087938ef72
Andrii <nagibator7337@gmail.com>
2022-08-27 19:27:22 +0300

    Added services impls.

--- CoffeShop/CoffeeShop.BusinessLogic/MainBusinessLogic/Services/OrderService.cs
+++ CoffeShop/CoffeeShop.BusinessLogic/MainBusinessLogic/Services/OrderService.cs
@@ -1,6 +1,56 @@
-﻿namespace CoffeeShop.BusinessLogic.MainBusinessLogic;
+﻿using CoffeeShop.DataAccess;
+using CoffeeShop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.BusinessLogic.MainBusinessLogic;
 
 public class OrderService
 {
     
+       private readonly CoffeeShopContext _context;
+       public OrderService(CoffeeShopContext context)
+       {   
+           _context = context;
+       } 
+       public async Task<List<Order>> GetAllAsync() => await _context.Orders
+           .Include(x=>x.OrderVolumeCoffees)
+           .ThenInclude(x=>x.Coffee)
+           
+           .Include(x=>x.OrderVolumeCoffees)
+           .ThenInclude(x=>x.Order)
+           
+           .Include(x=>x.OrderVolumeCoffees)
+           .ThenInclude(x=>x.Volume)
+           
+           .ToListAsync();
+
+       public async Task<Order?> GetAsync(int id) => await _context.Orders
+           .Include(x=>x.OrderVolumeCoffees)
+           .ThenInclude(x=>x.Coffee)
+           
+           .Include(x=>x.OrderVolumeCoffees)
+           .ThenInclude(x=>x.Order)
+           
+           .Include(x=>x.OrderVolumeCoffees)
+           .ThenInclude(x=>x.Volume)
+
+           .FirstOrDefaultAsync(x => x.Id.Equals(id));
+
+       public async Task CreateAsync(Order order)
+       {
+           await _context.Orders.AddAsync(order);
+           await _context.SaveChangesAsync();
+       }
+
+       public async Task UpdateAsync(Order order)
+       {
+           _context.Orders.Update(order);
+           await _context.SaveChangesAsync();
+       }
+
+       public async Task DeleteAsync(Order order)
+       {
+           _context.Orders.Remove(order);
+           await _context.SaveChangesAsync();
+       }
 }
--- CoffeShop/CoffeeShop.BusinessLogic/MainBusinessLogic/Services/VolumeService.cs
+++ CoffeShop/CoffeeShop.BusinessLogic/MainBusinessLogic/Services/VolumeService.cs
@@ -1,6 +1,49 @@
-﻿namespace CoffeeShop.BusinessLogic.MainBusinessLogic;
+﻿using CoffeeShop.DataAccess;
+using CoffeeShop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.BusinessLogic.MainBusinessLogic;
 
 public class VolumeService
 {
-    
+    private readonly CoffeeShopContext _context;
+    public VolumeService(CoffeeShopContext context)
+    {   
+        _context = context;
+    } 
+    public async Task<List<Volume>> GetAllAsync() => await _context.Volumes
+        .Include(x=>x.BonusCoffees)
+        .ThenInclude(x=>x.Coffee)    
+        
+        .Include(x=>x.OrderVolumeCoffees)
+        .ThenInclude(x=>x.Order)
+        
+        .ToListAsync();
+
+    public async Task<Volume?> GetAsync(int id) => await _context.Volumes
+        .Include(x=>x.BonusCoffees)
+        .ThenInclude(x=>x.Coffee)    
+        
+        .Include(x=>x.OrderVolumeCoffees)
+        .ThenInclude(x=>x.Order)
+        
+        .FirstOrDefaultAsync(x => x.Id.Equals(id));
+
+    public async Task CreateAsync(Volume volume)
+    {
+        await _context.Volumes.AddAsync(volume);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Volume volume)
+    {
+        _context.Volumes.Update(volume);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Volume volume)
+    {
+        _context.Volumes.Remove(volume);
+        await _context.SaveChangesAsync();
+    }
 }
