5d51ae67b4e83be2485a6aafca848bc57b6154a9
Andrii <nagibator7337@gmail.com>
2022-08-28 14:06:48 +0300

    VolumeController implementation.

--- CoffeShop/CoffeeShop.Api/Controllers/VolumeController.cs
+++ CoffeShop/CoffeeShop.Api/Controllers/VolumeController.cs
@@ -1,6 +1,55 @@
-﻿namespace CoffeShop.Api.Controllers;
+﻿using CoffeeShop.BusinessLogic.MainBusinessLogic.ServiceInterfaces;
+using CoffeeShop.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 
-public class VolumeController
+namespace CoffeShop.Api.Controllers;
+
+[ApiController]
+public class VolumeController  : ControllerBase
 {
+    private readonly ILogger<VolumeController> _logger;
+    private readonly IVolumeService _service;
+    public VolumeController(ILogger<VolumeController> logger,IVolumeService service)
+    {
+        _logger = logger;
+        _service = service;
+    }
+    [Route("")]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllAsync() 
+        => Ok(await _service.GetAllAsync());
+
+    [Route("{id}")]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAsync([FromRoute]int id) 
+        => Ok(await _service.GetAsync(id));
+
+    [Route("create")]
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<IActionResult> CreateAsync([FromBody]Volume volume)
+    {
+        await _service.CreateAsync(volume);
+        return Created(String.Empty,volume);
+    }
     
+    [Route("Update")]
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> UpdateAsync([FromBody]Volume volume)
+    {
+        await _service.UpdateAsync(volume);
+        return Ok();
+    }
+    
+    [Route("Delete")]
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> DeleteAsync([FromBody]Volume volume)
+    {
+        await _service.DeleteAsync(volume);
+        return Ok();
+    }
 }
