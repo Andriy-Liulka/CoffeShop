aa6b85789dbca4f222a28089b3834713f3fe0cfd
Andrii <nagibator7337@gmail.com>
2022-08-28 14:31:02 +0300

    DiscountController and OrderVolumeCoffee implementation.

--- CoffeShop/CoffeeShop.Api/Controllers/MtM_Controllers/DiscountCoffeeController.cs
+++ CoffeShop/CoffeeShop.Api/Controllers/MtM_Controllers/DiscountCoffeeController.cs
@@ -1,6 +1,55 @@
-﻿namespace CoffeShop.Api.Controllers.MtM_Controllers;
+﻿using CoffeeShop.BusinessLogic.MainBusinessLogic.ServiceInterfaces;
+using CoffeeShop.Domain.Entities.MtM_IntermediateEntities;
+using Microsoft.AspNetCore.Mvc;
 
-public class DiscountCoffeeController
+namespace CoffeShop.Api.Controllers.MtM_Controllers;
+
+[ApiController]
+public class DiscountCoffeeController  : ControllerBase
 {
+    private readonly ILogger<DiscountCoffeeController> _logger;
+    private readonly IDiscountCoffeeService _service;
+    public DiscountCoffeeController(ILogger<DiscountCoffeeController> logger,IDiscountCoffeeService service)
+    {
+        _logger = logger;
+        _service = service;
+    }
+    [Route("")]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllAsync() 
+        => Ok(await _service.GetAllAsync());
+
+    [Route("{id}")]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAsync([FromRoute]int coffeeId,[FromRoute]int discountId) 
+        => Ok(await _service.GetAsync(coffeeId,discountId));
+
+    [Route("create")]
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<IActionResult> CreateAsync([FromBody]DiscountCoffee discountCoffee)
+    {
+        await _service.CreateAsync(discountCoffee);
+        return Created(String.Empty,discountCoffee);
+    }
     
+    [Route("Update")]
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> UpdateAsync([FromBody]DiscountCoffee discountCoffee)
+    {
+        await _service.UpdateAsync(discountCoffee);
+        return Ok();
+    }
+    
+    [Route("Delete")]
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> DeleteAsync([FromBody]DiscountCoffee discountCoffee)
+    {
+        await _service.DeleteAsync(discountCoffee);
+        return Ok();
+    }
 }
--- CoffeShop/CoffeeShop.Api/Controllers/MtM_Controllers/OrderVolumeCoffeeController.cs
+++ CoffeShop/CoffeeShop.Api/Controllers/MtM_Controllers/OrderVolumeCoffeeController.cs
@@ -1,6 +1,55 @@
-﻿namespace CoffeShop.Api.Controllers.MtM_Controllers;
+﻿using CoffeeShop.BusinessLogic.MainBusinessLogic.ServiceInterfaces;
+using CoffeeShop.Domain.Entities.MtM_IntermediateEntities;
+using Microsoft.AspNetCore.Mvc;
 
-public class OrderVolumeCoffeeController
+namespace CoffeShop.Api.Controllers.MtM_Controllers;
+
+[ApiController]
+public class OrderVolumeCoffeeController : ControllerBase
 {
+    private readonly ILogger<OrderVolumeCoffeeController> _logger;
+    private readonly IOrderVolumeCoffeeService _service;
+    public OrderVolumeCoffeeController(ILogger<OrderVolumeCoffeeController> logger,IOrderVolumeCoffeeService service)
+    {
+        _logger = logger;
+        _service = service;
+    }
+    [Route("")]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllAsync() 
+        => Ok(await _service.GetAllAsync());
+
+    [Route("{id}")]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAsync([FromRoute]int orderId,[FromRoute]int volumeId,[FromRoute]int coffeeId) 
+        => Ok(await _service.GetAsync(orderId, volumeId, coffeeId));
+
+    [Route("create")]
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<IActionResult> CreateAsync([FromBody]OrderVolumeCoffee orderVolumeCoffee)
+    {
+        await _service.CreateAsync(orderVolumeCoffee);
+        return Created(String.Empty,orderVolumeCoffee);
+    }
     
+    [Route("Update")]
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> UpdateAsync([FromBody]OrderVolumeCoffee orderVolumeCoffee)
+    {
+        await _service.UpdateAsync(orderVolumeCoffee);
+        return Ok();
+    }
+    
+    [Route("Delete")]
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> DeleteAsync([FromBody]OrderVolumeCoffee orderVolumeCoffee)
+    {
+        await _service.DeleteAsync(orderVolumeCoffee);
+        return Ok();
+    }
 }
