117a799d55a2411aeae02724aa80fd12d5deb67f
Andrii <nagibator7337@gmail.com>
2022-08-28 13:52:50 +0300

    OrderController implementation.

--- CoffeShop/CoffeeShop.Api/Controllers/OrderController.cs
+++ CoffeShop/CoffeeShop.Api/Controllers/OrderController.cs
@@ -1,6 +1,55 @@
-﻿namespace CoffeShop.Api.Controllers;
+﻿using CoffeeShop.BusinessLogic.MainBusinessLogic.ServiceInterfaces;
+using CoffeeShop.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 
-public class OrderController
+namespace CoffeShop.Api.Controllers;
+
+[ApiController]
+public class OrderController : ControllerBase
 {
+    private readonly ILogger<OrderController> _logger;
+    private readonly IOrderService _service;
+    public OrderController(ILogger<OrderController> logger,IOrderService service)
+    {
+        _logger = logger;
+        _service = service;
+    }
+    [Route("")]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllAsync() 
+        => Ok(await _service.GetAllAsync());
+
+    [Route("{id}")]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAsync([FromRoute]int id) 
+        => Ok(await _service.GetAsync(id));
+
+    [Route("create")]
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<IActionResult> CreateAsync([FromBody]Order order)
+    {
+        await _service.CreateAsync(order);
+        return Created(String.Empty,order);
+    }
     
+    [Route("Update")]
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> UpdateAsync([FromBody]Order order)
+    {
+        await _service.UpdateAsync(order);
+        return Ok();
+    }
+    
+    [Route("Delete")]
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> DeleteAsync([FromBody]Order order)
+    {
+        await _service.DeleteAsync(order);
+        return Ok();
+    }
 }
