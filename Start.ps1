e9d42dd4c0300bcc96d3fef7e5721120ee0d3c84
AndriiLiulka <Andrii.Liulka@eleks.com>
2022-09-04 15:21:46 +0300

    Added script to automatical deploy.

---
+++ Start.ps1
@@ -0,0 +1,20 @@
+cd .\CoffeShop\CoffeeShop.DataAccess
+
+dotnet ef migrations add NewMigration -s ..\CoffeeShop.Api
+
+docker pull mcr.microsoft.com/mssql/server:2022-latest
+
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Aa123456789" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2022-latest
+
+dotnet ef database update -s ..\CoffeeShop.Api
+
+cd ..\..
+
+
+ docker build  -f ".\CoffeShop\CoffeeShop.Api\Dockerfile"  -t aspnetcont:dev .
+
+docker run -d -p 8080:80 --name myapp aspnetcont:dev 
+
+
+
+
