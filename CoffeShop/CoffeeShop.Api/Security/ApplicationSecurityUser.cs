f01df479835078173ccf0c76552e3882a80845b2
AndriiLiulka <Andrii.Liulka@eleks.com>
2022-09-05 23:28:28 +0300

    First security impl commit.

---
+++ CoffeShop/CoffeeShop.Api/Security/ApplicationSecurityUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CoffeShop.Api.Security;
+
+public class ApplicationSecurityUser : IdentityUser
+{
+    public string? RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiryTime { get; set; }
+}
---
+++ CoffeShop/CoffeeShop.Api/Security/AuthModels/CommonModels/TokenModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CoffeShop.Api.Security.AuthModels.CommonModels;
+
+public class TokenModel
+{
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
