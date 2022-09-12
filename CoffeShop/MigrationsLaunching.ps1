5d104d9bf5cd63901a03ac262bfbeea36444d31b
AndriiLiulka <Andrii.Liulka@eleks.com>
2022-09-12 17:13:31 +0300

    Script to launch migration.

---
+++ CoffeShop/MigrationsLaunching.ps1
@@ -0,0 +1,10 @@
+
+cd ./CoffeeShop.DataAccess/
+
+dotnet ef database drop -f -v -s ..\CoffeeShop.Api\
+
+Remove-Item Migrations
+
+dotnet ef migrations add Migration1 -s  ..\CoffeeShop.Api\
+
+dotnet ef database update -s ..\CoffeeShop.Api\
