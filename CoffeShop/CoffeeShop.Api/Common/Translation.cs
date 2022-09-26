b16d4a17af5a1b4d56bd3d54948bcd7543f7a9a6
AndriiLiulka <Andrii.Liulka@eleks.com>
2022-09-26 23:38:11 +0300

    Added translations.

--- CoffeShop/CoffeeShop.Api/Common/Translation.cs
+++ CoffeShop/CoffeeShop.Api/Common/Translation.cs
@@ -5,4 +5,8 @@ public static class Translation
     public const string EmptyStringMessage = "Your connection string is empty";
     public const string BadRequestObjectResultMessage = "Type-> {0} \n Message-> {1}";
     public const string ErrorMiddlewareHandlingMessage = "Error happens during request \"{0}\"";
+    
+    public const string DefaultName = "Empty name";
+    public const string DefaultEmail = "Empty email";
+    public const string DefaultRole = "Empty role";
 }
