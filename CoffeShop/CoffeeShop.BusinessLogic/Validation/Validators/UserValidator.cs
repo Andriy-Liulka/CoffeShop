4411bc2fab62ca7cc0633c1ae501502580a9711c
AndriiLiulka <Andrii.Liulka@eleks.com>
2022-10-18 23:45:34 +0300

    Added UserValidator.

--- CoffeShop/CoffeeShop.BusinessLogic/Validation/Validators/UserValidator.cs
+++ CoffeShop/CoffeeShop.BusinessLogic/Validation/Validators/UserValidator.cs
@@ -1,4 +1,5 @@
-﻿using CoffeeShop.Domain.Entities.Identity;
+﻿using CoffeeShop.BusinessLogic.Validation.Common;
+using CoffeeShop.Domain.Entities.Identity;
 using FluentValidation;
 
 namespace CoffeeShop.BusinessLogic.Validation.Validators;
@@ -7,6 +8,9 @@ public class UserValidator : AbstractValidator<User> , ICustomValidator
 {
     public UserValidator()
     {
-            
+        RuleFor(x => x.LastName).NotEmpty().NotNull().Must(CustomRules.StringLengthRule);
+        RuleFor(x => x.FirstName).NotEmpty().NotNull().Must(CustomRules.StringLengthRule);
+        RuleFor(x => x.Login).NotEmpty().NotNull().Must(CustomRules.StringLengthRule);
+        RuleFor(x => x.Email).NotEmpty().NotNull().Must(CustomRules.StringLengthRule);
     }
 }
